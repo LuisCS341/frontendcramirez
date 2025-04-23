240c7f19a2750f9f7b650f0c9910af8a2d5aac65
HP <luis.cisneros.salas03@gmail.com>
2025-04-23 05:23:38 -0500

    "change routes"

--- src/components/dashboard/BarraLateralDashboard.vue
+++ src/components/dashboard/BarraLateralDashboard.vue
@@ -25,7 +25,7 @@
 
             <!-- Mostrar "Mis Clientes" solo si NO eres Jefe -->
             <li v-if="userRole === 2 || userRole === 3">
-              <router-link to="/dashboard/Clientes">
+              <router-link to="/dashboard/clientes">
                 <img :src="icons['simbolo-de-hombre-y-flecha-derecha.png']" alt="Mis Clientes" class="icono" />
                 Mis Clientes
               </router-link>
