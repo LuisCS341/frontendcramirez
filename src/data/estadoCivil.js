d5d0b49398d0c6555c9b1c44b9a70725523655cf
HP <luis.cisneros.salas03@gmail.com>
2025-05-12 18:18:34 -0500

    "add new date viudo/a"

--- src/data/estadoCivil.js
+++ src/data/estadoCivil.js
@@ -2,5 +2,6 @@
 export const estadoCivil = [
     { id: 1, nombre: 'Soltero' },
     { id: 2, nombre: 'Casado' },
-    { id: 3, nombre: 'Divorciado' }
+    { id: 3, nombre: 'Divorciado' },
+    { id: 4, nombre: 'Viudo/a' }
 ];
