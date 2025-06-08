8cb9a9c66881b4df0147162e32c31bb06011596a
HP <luis.cisneros.salas03@gmail.com>
2025-06-08 06:15:44 -0500

    add new stationary data

--- src/data/estadoCivil.js
+++
@@ -1,9 +0,0 @@
-
-export const estadoCivil = [
-    { id: 1, nombre: 'SOLTERA(O)' },
-    { id: 2, nombre: 'CASADA(O)' },
-    { id: 3, nombre: 'DIVOCIADA(O)' },
-    { id: 4, nombre: 'VIUDA(O)' },
-    { id: 5, nombre: 'ME ABSTENGO' },
-    { id: 5, nombre: '-' }
-];
--- src/data/tipoIdentificacion.js
+++
@@ -1,8 +0,0 @@
-export const tipoIdentificacion = [
-    { id: 1, nombre: 'DNI' },
-    { id: 2, nombre: 'RUC' },
-    { id: 3, nombre: 'CE' },
-    { id: 4, nombre: 'C.E' },
-    { id: 5, nombre: 'C.E.' },
-    { id: 6, nombre: 'PASAPORTE' }
-];
