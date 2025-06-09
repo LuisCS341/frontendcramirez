58171c7e1b6f463f1aa13b55130a6fb6793e71bb
HP <luis.cisneros.salas03@gmail.com>
2025-06-08 21:40:46 -0500

    change C.E.

--- src/data/tipoIdentificacion.js
+++ src/data/tipoIdentificacion.js
@@ -1,8 +1,7 @@
 export const tipoIdentificacion = [
     { id: 1, nombre: 'DNI' },
     { id: 2, nombre: 'RUC' },
-    { id: 3, nombre: 'CE' },
-    { id: 4, nombre: 'C.E' },
-    { id: 5, nombre: 'C.E.' },
-    { id: 6, nombre: 'PASAPORTE' }
+    { id: 3, nombre: 'C.E' },
+    { id: 4, nombre: 'PASAPORTE' },
+    { id: 5, nombre: '-' }
 ];
