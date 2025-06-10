674a3d42df3bf8ecee409a25f04b87887a2ce63f
HP <luis.cisneros.salas03@gmail.com>
2025-06-10 13:31:23 -0500

    add new options on tiposContrato

--- src/data/tiposContrato.js
+++ src/data/tiposContrato.js
@@ -2,5 +2,6 @@ export const tiposContrato = [
     { id: 1, nombre: 'T1' },
     { id: 2, nombre: 'T2' },
     { id: 3, nombre: 'T3' },
-    { id: 4, nombre: '-' },
+    { id: 4, nombre: 'LEGAL' },
+    { id: 5, nombre: '-' },
 ];
