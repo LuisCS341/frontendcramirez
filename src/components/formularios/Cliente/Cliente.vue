9608fe16cfc581fc61d1be75fcf57869e7132e42
javier <jsilvab14@gmail.com>
2025-06-19 13:34:19 -0500

    correccion formulario cliente datos personales

--- src/components/formularios/Cliente/Cliente.vue
+++ src/components/formularios/Cliente/Cliente.vue
@@ -1,112 +1,104 @@
 <template>
-  <div v-if="form">
-    <h3>Datos Personales</h3>
+  <div v-if="form" class="container">
+    <div class="formulario-all">
+      <h3>Datos Personales</h3>
+      <form>
+        <div>
+          <label>Tipo de Identificación:</label>
+          <select v-model="form.tipoIdentificacion" required>
+            <option v-for="tipo in tipoIdentificacion" :key="tipo.id" :value="tipo.id">{{ tipo.nombre }}</option>
+          </select>
+        </div>
 
-    <label>Tipo de Identificación:</label>
+        <div>
+          <label>Número de Identificación:</label>
+          <input v-model="form.numIdentificacionUsuario" type="text" required maxlength="8" @input="form.numIdentificacionUsuario = form.numIdentificacionUsuario.replace(/[^0-9]/g, '')" />
+        </div>
 
-    <select v-model="form.tipoIdentificacion" required>
-      <option v-for="tipo in tipoIdentificacion" :key="tipo.id" :value="tipo.id">{{ tipo.nombre }}</option>
-    </select>
+        <div style="grid-column: span 2">
+          <label>Apellido y Nombre:</label>
+          <input v-model="form.nombreCliente" type="text" required @input="form.nombreCliente = form.nombreCliente.replace(/[0-9]/g, '')" />
+        </div>
 
-    <label>Número de Identificación:</label>
-    <input
-        v-model="form.numIdentificacionUsuario"
-        type="text"
-        required
-        maxlength="8"
-        @input="form.numIdentificacionUsuario = form.numIdentificacionUsuario.replace(/[^0-9]/g, '')"
-    />
+        <div style="grid-column: span 2">
+          <label>Ocupación:</label>
+          <input v-model="form.ocupacionCliente" type="text" required placeholder="Ingrese su Ocupación" @input="form.ocupacionCliente = form.ocupacionCliente.replace(/[0-9]/g, '')" />
+        </div>
 
-    <label>Apellido y Nombre:</label>
-    <input
-        v-model="form.nombreCliente"
-        type="text"
-        required
-        @input="form.nombreCliente = form.nombreCliente.replace(/[0-9]/g, '')"
-    />
+        <div>
+          <label>País de Origen:</label>
+          <select v-model="form.nacionalidad" required>
+            <option disabled value="">Seleccione un país</option>
+            <option v-for="pais in nacionalidad" :key="pais.id" :value="pais.id">{{ pais.nombre }}</option>
+          </select>
+        </div>
 
-    <label>Ocupación:</label>
-    <input
-        v-model="form.ocupacionCliente"
-        type="text"
-        required
-        placeholder="Ingrese su Ocupación"
-        @input="form.ocupacionCliente = form.ocupacionCliente.replace(/[0-9]/g, '')"
-    />
+        <div>
+          <label>País de Residencia:</label>
+          <select v-model="form.paisdeResidencia" required>
+            <option disabled value="">Seleccione un país</option>
+            <option v-for="residencia in residencias" :key="residencia.id" :value="residencia.id">{{ residencia.nombre }}</option>
+          </select>
+        </div>
 
+        <div>
+          <label>Departamento:</label>
+          <select v-model="form.departamento" required>
+            <option disabled value="">Selecciona un departamento</option>
+            <option v-for="departamento in departamentos" :key="departamento.id" :value="departamento.id">{{ departamento.nombre }}</option>
+          </select>
+        </div>
 
-    <label for="paisOrigen">País de Origen:</label>
-    <select v-model="form.nacionalidad" required>
-      <option disabled value="">Seleccione un país</option>
-      <option v-for="pais in nacionalidad" :key="pais.id" :value="pais.id">{{ pais.nombre }}</option>
-    </select>
+        <div>
+          <label>Provincia:</label>
+          <select v-model="form.provincia" required>
+            <option disabled value="">Seleccione una provincia</option>
+            <option v-for="provincia in provinciasFiltradas" :key="provincia.id" :value="provincia.id">{{ provincia.nombre }}</option>
+          </select>
+        </div>
 
-    <label for="paisResidencia">País de Residencia:</label>
-    <select v-model="form.paisdeResidencia" required>
-      <option disabled value="">Seleccione un país</option>
-      <option v-for="residencia in residencias" :key="residencia.id" :value="residencia.id">{{ residencia.nombre }}</option>
-    </select>
+        <div>
+          <label>Distrito:</label>
+          <select v-model="form.distrito" required>
+            <option disabled value="">Seleccione un distrito</option>
+            <option v-for="distrito in distritosFiltrados" :key="distrito.id" :value="distrito.id">{{ distrito.nombre }}</option>
+          </select>
+        </div>
 
-    <label for="departamento">Departamento:</label>
-    <select v-model="form.departamento" required>
-      <option disabled value="">Selecciona un departamento</option>
-      <option v-for="departamento in departamentos" :key="departamento.id" :value="departamento.id">{{ departamento.nombre }}</option>
-    </select>
+        <div style="grid-column: span 2">
+          <label>Dirección:</label>
+          <input v-model="form.direccion" type="text" required placeholder="Ingrese su Dirección" />
+        </div>
 
-    <label>Provincia:</label>
-    <select v-model="form.provincia" required>
-      <option disabled value="">Seleccione una provincia</option>
-      <option v-for="provincia in provinciasFiltradas" :key="provincia.id" :value="provincia.id">{{ provincia.nombre }}</option>
-    </select>
+        <div style="grid-column: span 2">
+          <label>Correo Electrónico:</label>
+          <input v-model="form.correoUsuario" type="text" required placeholder="usuario@dominio.com" />
+        </div>
 
-    <label>Distrito:</label>
-    <select v-model="form.distrito" required>
-      <option disabled value="">Seleccione un distrito</option>
-      <option v-for="distrito in distritosFiltrados" :key="distrito.id" :value="distrito.id">{{ distrito.nombre }}</option>
-    </select>
+        <div>
+          <label>Número Telefónico:</label>
+          <div class="prefijo-cliente">
+            <select v-model="form.prefijoTelefonico" required>
+              <option v-for="prefijo in prefijos" :key="prefijo.id" :value="prefijo.id">{{ prefijo.codigo }}</option>
+            </select>
+            <input v-model="form.numTelefonico" type="text" placeholder="Ingrese su Número" required @input="form.numTelefonico = form.numTelefonico.replace(/[^0-9]/g, '')" />
+          </div>
+        </div>
 
-    <label>Dirección:</label>
-    <input
-        v-model="form.direccion"
-        type="text"
-        required
-        placeholder="Ingrese su Dirección"
-    />
+        <div>
+          <label>Estado Civil:</label>
+          <select v-model="form.estadoCivil" required>
+            <option v-for="estado in estadoCivil" :key="estado.id" :value="estado.id">{{ estado.nombre }}</option>
+          </select>
+        </div>
 
-    <label>Correo Electrónico:</label>
-    <input
-        v-model="form.correoUsuario"
-        type="text"
-        required
-        placeholder="usuario@dominio.com"
-    />
+        <div>
+          <label>Descripción de Estado Civil:</label>
+          <input v-model="form.descripcionEstadoCivil" type="text" required placeholder="Ingrese la Descripcion de Estado Civil" />
+        </div>
 
-    <label for="prefijoTelefonico">Número Telefónico:</label>
-    <div class="prefijo-cliente">
-      <select v-model="form.prefijoTelefonico" required>
-        <option v-for="prefijo in prefijos" :key="prefijo.id" :value="prefijo.id">{{ prefijo.codigo }}</option>
-      </select>
-      <input
-          v-model="form.numTelefonico"
-          type="text"
-          placeholder="Ingrese su Número"
-          required
-          @input="form.numTelefonico = form.numTelefonico.replace(/[^0-9]/g, '')"
-      />
+      </form>
     </div>
-
-    <label for="estadoCivil">Estado Civil:</label>
-    <select v-model="form.estadoCivil" required>
-      <option v-for="estado in estadoCivil" :key="estado.id" :value="estado.id">{{ estado.nombre }}</option>
-    </select>
-
-    <label>Descripcion de Estado Civil:</label>
-    <input
-        v-model="form.descripcionEstadoCivil"
-        type="text"
-        required
-        placeholder="Ingrese la Descripcion de Estado Civil"
-    />
   </div>
 </template>
 
