3f46a654d4ee304584116df94b948605158c8184
javier <jsilvab14@gmail.com>
2025-06-28 02:11:14 -0500

    bancos v1

---
+++ src/data/Bancos.js
@@ -0,0 +1,18 @@
+export const Bancos = [
+    { id: 1, nombre: 'Banco de Crédito del Perú (BCP)' },
+    { id: 2, nombre: 'BBVA Perú' },
+    { id: 3, nombre: 'Interbank' },
+    { id: 4, nombre: 'Scotiabank Perú' },
+    { id: 5, nombre: 'Banco Pichincha Perú' },
+    { id: 6, nombre: 'Banco GNB Perú' },
+    { id: 7, nombre: 'Banco Falabella' },
+    { id: 8, nombre: 'Banco Ripley' },
+    { id: 9, nombre: 'Banco Azteca Perú' },
+    { id: 10, nombre: 'MiBanco' },
+    { id: 11, nombre: 'Banco de la Nación' },
+    { id: 12, nombre: 'Banco Cencosud' },
+    { id: 13, nombre: 'Banco Santander Perú' },
+    { id: 14, nombre: 'Banco Comercio' },
+    { id: 15, nombre: 'Banco ICBC Perú' },
+    { id: 16, nombre: 'Otros' }
+];
