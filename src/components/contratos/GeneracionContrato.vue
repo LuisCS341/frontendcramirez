c31cb3ea9a22f11bf9ede6f631f28c222f570ce4
javier <jsilvab14@gmail.com>
2025-08-12 18:34:54 -0500

    datosss

--- src/components/contratos/GeneracionContrato.vue
+++ src/components/contratos/GeneracionContrato.vue
@@ -319,9 +319,7 @@ const descargarWordT1 = async (cliente) => {
           })()
           : '-',
       costoLote: lote?.costoLote ? parseFloat(lote.costoLote).toLocaleString('en-US', { minimumFractionDigits: 2, maximumFractionDigits: 2 }) : '-',
-      precioMetroCuadrado: lote?.precioMetroCuadrado
-          ? parseFloat(lote.precioMetroCuadrado).toLocaleString('en-US', { minimumFractionDigits: 2, maximumFractionDigits: 2 })
-          : '-',
+      precioMetroCuadrado: lote?.precioMetroCuadrado ? parseFloat(lote.precioMetroCuadrado).toLocaleString('en-US', { minimumFractionDigits: 2, maximumFractionDigits: 2 }) : '-',
       precioMetroCuadradoLetras:lote?.precioMetroCuadradoLetras??'-',
       costoLoteLetras: (lote?.costoLoteLetras ?? '-').toUpperCase(),
       areaLote: lote?.areaLote ? parseFloat(lote.areaLote).toFixed(2) : '-',
@@ -342,9 +340,7 @@ const descargarWordT1 = async (cliente) => {
       urbanizacionMatriz:matriz?.urbanizacionMatriz?? '-',
       compraventaMatriz:matriz?.compraventaMatriz?? '-',
       situacionLegalMatriz: matriz?.situacionLegalMatriz?? '-',
-      cuotaInicialIncluyeSeparacion: cuota?.cuotaInicialIncluyeSeparacion
-          ? parseFloat(cuota.cuotaInicialIncluyeSeparacion).toLocaleString('en-US', { minimumFractionDigits: 2, maximumFractionDigits: 2 })
-          : '-',
+      cuotaInicialIncluyeSeparacion: cuota?.cuotaInicialIncluyeSeparacion ? parseFloat(cuota.cuotaInicialIncluyeSeparacion).toLocaleString('en-US', { minimumFractionDigits: 2, maximumFractionDigits: 2 }) : '-',
       cuotaInicialIncluyeSeparacionLetras:cuota?.cuotaInicialIncluyeSeparacionLetras?? '-',
       fechaPago: cuota?.fechaPago?? '-',
       cuentaRecaudadora: cuota?.cuentaRecaudadora?? '-',
