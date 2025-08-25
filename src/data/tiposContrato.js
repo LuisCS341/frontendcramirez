54bf0c5059790ad09395b1bfcf3fcd039bfd4d56
HP <luis.cisneros.salas03@gmail.com>
2025-08-24 20:11:58 -0500

    se cambio tiposcontrato

--- src/data/tiposContrato.js
+++ src/data/tiposContrato.js
@@ -1,7 +1,5 @@
 export const tiposContrato = [
     { id: 1, nombre: 'T1' },
     { id: 2, nombre: 'T2' },
-    { id: 3, nombre: 'T3' },
-    { id: 4, nombre: 'LEGAL' },
-    { id: 5, nombre: '-' },
+    { id: 3, nombre: 'T3' }
 ];
